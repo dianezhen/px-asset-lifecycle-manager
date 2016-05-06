62c151702f4bd4a5a95a28209511178e0cc5f915
dianezhen <diane.zhen@ge.com>
2016-05-06 11:36:42 -0400

    Took version from CleanupForBootstrapIntegration branch

--- px-asset-lifecycle-manager/src/main/java/com/ge/predix/alm/AlmApplication.java
+++ px-asset-lifecycle-manager/src/main/java/com/ge/predix/alm/AlmApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-@EnableResourceServer
 @ComponentScan
 public class AlmApplication {
 
